e6ba0816ea5322e584745ecbde52b2c5e3d408d3
Iamneo <techadmin@iamneo.ai>
2023-09-13 06:35:59 +0000

    Backup Commit

--- 5.ps1
+++ 5.ps1
@@ -1,7 +1,12 @@
 new-item Testing itemtype directory
 cd Testing
 new-item Subfolder1 itemtype directory
-cd Subfolder1 
+
 new-item Test1.txt
-new-item T
+new-item Test2.txt
+new-item Test3.txt
 new-item Subfolder2 itemtype directory
+
+new-item Test4.txt
+new-item Test5.txt
+new-item Test6.txt
